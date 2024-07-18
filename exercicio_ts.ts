6aa6219988774f3c2a0fef07d04bc69ab6fef982
IuryyCosta <iury.costa2209@gmail.com>
2024-07-18 20:49:23 -0300

    Create exercicio_ts.ts

---
+++ exercicio_ts.ts
@@ -0,0 +1,13 @@
+// Função de multiplicação
+function multiplicacao(a: number, b: number): number {
+    return a * b;
+}
+
+// Função de saudação
+function saudacao(nome: string): string {
+    return "Olá " + nome;
+}
+
+// Exemplos de uso das funções
+console.log(multiplicacao(5, 3)); // Saída: 15
+console.log(saudacao("Maria")); // Saída: Olá Maria
